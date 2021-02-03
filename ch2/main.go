9a9fbbe8ec8b9e9e6330a98dd7a2152475e26748
caozhijian <1012875698@qq.com>
2021-02-03 16:12:12 +0800

    变量的定义

---
+++ ch2/main.go
@@ -0,0 +1,36 @@
+package main
+
+func main() {
+	//变量的定义
+	//1.最基础的变量定义
+	//var i int
+	//i = 20
+	//fmt.Println(i)
+	//定义并初始化
+	//var i int = 10
+	//fmt.Println(i)
+	//2. 根据值自行推断变量类型
+	//var i = 100 //没有设置类型
+	//fmt.Println(i)
+	//3. 省略var
+	//i :=100
+	//fmt.Println(i)
+	//var a int = 1
+	//var b = 2
+	//c := 3
+	//fmt.Println(a,b,c)
+
+	//多变量定义
+	//var a,b,c int
+	//a,b,c = 10,20,30
+	//fmt.Println(a,b,c)
+
+	//var a,b,c int = 10,20,30
+	//fmt.Println(a,b,c)
+
+	//集合类型
+	//var (
+	//	a int
+	//	name string
+	//)
+}
