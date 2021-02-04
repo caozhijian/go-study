8642d1201f63fad92283ee0547ec2dcdce71048a
caozhijian <1012875698@qq.com>
2021-02-04 12:27:44 +0800

    常见数据类型-float

--- ch4/main.go
+++ ch4/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"unsafe"
 )
 
@@ -13,4 +14,12 @@ func main() {
 
 	var age int = 18
 	fmt.Println(unsafe.Sizeof(age))
+
+	//float类型
+	var weight float32 = 92.5
+	fmt.Println(weight)
+	fmt.Println(math.MaxFloat32)
+
+	weightFree := 80.5
+	fmt.Printf("%T", weightFree) //Output: float64
 }
