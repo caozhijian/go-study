d54f9a0f3205e0a8096c451bd6f0fe0f9ce0a4f7
caozhijian <1012875698@qq.com>
2021-02-05 12:22:18 +0800

    slice的声明与初始化

---
+++ mapAndSlice/slice_test/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	courses := []string{"语文", "英语", "历史"}
+	fmt.Printf("courses的类型是%T\n", courses) //Output: courses的类型是[]string
+
+	//使用make对slice进行声明与初始化
+	courses2 := make([]string, 5)
+	courses2 = []string{"语文", "英语", "历史"}
+	fmt.Println(courses2)
+
+	//通过数组的方式生成切片
+	course3 := [5]string{"数学", "物理", "化学", "生物", "小提琴"}
+	subCourse3 := course3[1:4]
+	fmt.Println(subCourse3)
+	fmt.Printf("sunCourse3的类型是%T", subCourse3)
+
+}
