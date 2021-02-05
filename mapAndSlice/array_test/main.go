3e8ff4578bb8bd02161347acbd0a4e1c246e4d78
caozhijian <1012875698@qq.com>
2021-02-05 11:41:20 +0800

    数组是值类型

--- mapAndSlice/array_test/main.go
+++ mapAndSlice/array_test/main.go
@@ -27,4 +27,18 @@ func main() {
 		sum += value
 	}
 	fmt.Println(sum)
+
+	//数组是值类型
+	courseA := [3]string{"语文", "英语", "历史"}
+	courseB := [...]string{"数学", "物理", "化学", "生物"}
+	fmt.Printf("CourseA的类型是%T\n", courseA)
+	fmt.Printf("CourseB的类型是%T\n", courseB)
+	//在函数传递参数时，数组作为参数，实际调用的时候是值传递
+	printArray(courseB)
+	fmt.Println(courseB) //Output:[数学 物理 化学 生物]
+
+}
+func printArray(toPrint [4]string) {
+	toPrint[0] = "体育"
+	fmt.Println(toPrint) //Output: [体育 物理 化学 生物]
 }
