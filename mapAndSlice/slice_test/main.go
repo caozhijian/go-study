437659ee2f08f38a7de19a0435b0282e50939f15
caozhijian <1012875698@qq.com>
2021-02-05 12:58:46 +0800

    slice基本操作

--- mapAndSlice/slice_test/main.go
+++ mapAndSlice/slice_test/main.go
@@ -20,6 +20,29 @@ func main() {
 	//数组是值传递，但切片是引用传递
 	replace(subCourse3)
 	fmt.Println(subCourse3) //Output: [逗你玩 化学 生物]
+
+	//append,可以向slice追加元素
+	subCourse3 = append(subCourse3, "相声")
+	fmt.Println(subCourse3)
+
+	//copy函数,拷贝时，需要设置好目标对象长度
+	//subCourse4 := []string{}
+	subCourse4 := make([]string, len(subCourse3))
+	copy(subCourse4, subCourse3)
+	fmt.Println(subCourse4)
+
+	//合并两个切片
+	slice1 := []string{"slice1"}
+	slice2 := []string{"slice2"}
+	subCourse5 := append(slice1, slice2...)
+	fmt.Println(subCourse5)
+
+	//删除元素
+	deleteCourses := [5]string{"数学", "物理", "化学", "生物", "小提琴"}
+	courseSlice := deleteCourses[:]
+	courseSlice = append(courseSlice[:1], courseSlice[2:]...) //删除物理
+	fmt.Println(courseSlice)
+
 }
 
 func replace(mySlice []string) {
