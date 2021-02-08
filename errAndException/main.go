bf0ecf0a53e24f2416e3970c2cc583e9df450de8
caozhijian <1012875698@qq.com>
2021-02-08 12:33:49 +0800

    panic与recover

--- errAndException/main.go
+++ errAndException/main.go
@@ -1,1 +1,22 @@
 package main
+
+import "fmt"
+
+func main() {
+	a := 12
+	b := 0
+	defer func() {
+		err := recover()
+		if err != nil {
+			fmt.Println("捕获到异常")
+		}
+		fmt.Println("Hello")
+	}()
+	fmt.Println(div(a, b))
+}
+func div(a, b int) (int, error) {
+	if b == 0 {
+		panic("被除数不能为0")
+	}
+	return a / b, nil
+}
